3a20a8a80140cfde67ed3597f82009a015f321e8
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-05-12 11:04:06 -0500

    ⚡1

---
+++ reading_documention_2/style_guide.js
@@ -0,0 +1,9 @@
+let iceCreamTaste = "chocolate";
+let iceCreamDensity = 10;
+
+while (iceCreamDensity > 0) {
+  console.log(`Drip... ${iceCreamDensity}`);
+  --iceCreamDensity;
+}
+
+console.log(`The ${iceCreamTaste} ice cream melted.`);
