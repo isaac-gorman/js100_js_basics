efa9cdb96f7ef9dc3fbe3d2da59462da6e1f0e6b
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-05-12 14:10:09 -0500

    ⚡4

---
+++ reading_documention_2/string_concatenation.js
@@ -0,0 +1,7 @@
+let str1 = "My favorite movie is Rudy";
+let str2 = "I love theme parks!";
+let str3 = "I have a dog named Opie.";
+
+let concatenation1 = (str1, str2, str3) => str1 + ". " + str2 + ". " + str3;
+
+console.log(concatenation1(str1, str2, str3));
