89cba0dc2bc3e014e920cb15bea93795740dffae
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-05-12 09:36:16 -0500

    ⚡3

--- reading_documention_1/mountian.js
+++ reading_documention_1/mountian.js
@@ -1,9 +1,4 @@
 let strA = "mountain";
-let upperCaseFirstLetter = (str) => {
-  let newArray = str.split("");
-  newArray[0] = newArray[0].toUpperCase();
-  let newString = newArray.join("");
-  return newString;
-};
+let upperCaseFirstLetter = (str) => str[0].toUpperCase() + str.substring(1);
 
 console.log(upperCaseFirstLetter(strA));
