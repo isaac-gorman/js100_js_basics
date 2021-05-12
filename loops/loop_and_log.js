3839695699726dab53cd349454660143ba861781
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-05-12 14:23:07 -0500

    ⚡1

---
+++ loops/loop_and_log.js
@@ -0,0 +1,11 @@
+// With initial expression, condition, and incrementExpression
+for (let i = 0; i <= 10; ) {
+  console.log(i);
+  i += 2;
+}
+
+// Without incrementExpression
+for (let i = 0; i <= 10; ) {
+  console.log(i);
+  i += 2;
+}
