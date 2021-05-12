95c7fa7892b5781a51ce6a269485210742f9bad9
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-05-12 16:26:19 -0500

    ⚡7

---
+++ loops/finding_nemo.js
@@ -0,0 +1,11 @@
+let fish = ["Dory", "Marlin", "Gill", "Nemo", "Bruce"];
+
+for (let i = 0; i <= fish.indexOf("Nemo"); i++) console.log(fish[i]);
+
+// for (let i = 0; i < fish.length; i++) {
+//   console.log(fish[i]);
+
+//   if (fish[i] === "Nemo") {
+//     break;
+//   }
+// }
